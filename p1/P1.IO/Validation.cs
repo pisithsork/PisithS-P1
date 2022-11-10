536617a0a6eeb13648caafba8f0ba8b53db1a42d
pisithsork <55104592+pisithsork@users.noreply.github.com>
2022-11-10 18:50:30 -0500

    Added new Folders and made Method in IO

---
+++ p1/P1.IO/Validation.cs
@@ -0,0 +1,122 @@
+﻿using P1.Data;
+using System.Net;
+
+namespace P1.IO
+{
+    public class Validation
+    {
+        //Fields
+        Dictionary <string, string> Credentials = new Dictionary <string, string>();
+
+        //Constructors
+        public Validation () { }
+
+        public Validation (Dictionary<string, string> credentials)
+        {
+            /*this.userName = newuser.getUserName();
+            this.Password = newuser.getPassword();*/
+            this.Credentials = credentials;
+        }
+
+        //Methods
+
+        //I wanna method to check to see if email exist within the database(dictionary)
+        public bool doesEmailExist(string userName)
+        {
+            if (Credentials.ContainsKey(userName))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //I wanna method to validate if the credentials entered exist within the database(dictionary)
+        public bool isCredentialValid(string userName, string Password, Dictionary<string, string> credentials)
+        {
+            if (credentials.ContainsKey(userName))
+            {
+                if (credentials[userName] == Password)
+                {
+                    Console.WriteLine("Credentials Accepted!");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("The email or password was incorrect please try again");
+                    return false;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("The email or password was incorrect please try again");
+                return false;
+            }
+        }
+        //I wanna method to take in Valid Input
+        public (string,string) getUserInput()
+        {
+            string inputUsername = "";
+            string inputPassword = "";
+            bool isValid = true;
+            bool isEmailUnique = true;
+
+            while (isValid)
+            {
+                Console.WriteLine("Please enter your email");
+                Console.Write("Email: ");
+                inputUsername = Console.ReadLine();
+                Console.Write("Password: ");
+                inputPassword = Console.ReadLine();
+
+                if(!(String.IsNullOrEmpty(inputUsername)) && !(String.IsNullOrEmpty(inputPassword)))
+                {
+                    isValid = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please re-enter valid username AND password!");
+                }
+            }
+            return (inputUsername, inputPassword);
+        }
+        //I wanna have a method to register new user
+        public bool getRegistration()
+        {
+            bool isValidEmail = true;
+            while (isValidEmail)
+            {
+                var UserInput = getUserInput();
+                if (!doesEmailExist(UserInput.Item1))
+                {
+                    isValidEmail = false;
+                    Credentials.Add(UserInput.Item1, UserInput.Item2);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Registration did not work end program and fix bug");
+                    isValidEmail = false;
+                }
+            }
+            return false;
+        }
+        //I wanna get a method to login an existing user
+        public bool getLogin()
+        {
+            
+            bool loops = true;
+            while (loops)
+            {
+                var UserInput = getUserInput();
+                bool answer = isCredentialValid(UserInput.Item1, UserInput.Item2, Credentials);
+                if (answer)
+                {
+                    Console.WriteLine("Login Accepted!");
+                    return true;
+                }
+            }
+            return false;
+
+        } 
+    }
+}
--- p1/program/Program.cs
+++ p1/program/Program.cs
@@ -1,48 +1,51 @@
-﻿
+﻿using P1.Data;
+using P1.IO;
+using System;
 
 public class Program
 {
-
+    //! NOTES:
+    //! 1. The login and register method works fine IF I input the value correct the first time BUT If say I user input incorrectly the first time, it loops to asking the email and password from user over and over
+    //! 2. Implement a way to user database and sql IF it is possible based on the current state of the program. There would be no reason to implement something that the foundation of the structure has not been made yet
+    //! 3. Use the Dictionary to create a database and refractor all use of dictionary
+    //! 4. Create a LoginOrRegister() Method in Validation.cs class that outputs either 1 or 2 to decide if user will login or register
     public static void Main(String[] args)
     {
-
         /* We are creating a login/register program, one that is stable enough to be able to fall back to
          *  it is not a necessity to create it as efficient as possible because it is only to be meant as
          *  as a state of the program to fall back to if ever need to redo and rework testing programs
          */
-
-        // test case
         string userName = "pisithsork@yahoo.com";
         string password = "123456";
-        //string value = "";
         bool looping = true;
-        Dictionary<string, string> Credentials = new Dictionary<string, string>();
-        Credentials.Add(userName, password);
+        Dictionary<string, string> tmp = new Dictionary<string, string>();
+        tmp.Add(userName, password);
+        Validation Credentials = new Validation(tmp);
 
         //Create a prompt for user to either login or register
-        
         //Checks to see if user input is valid of either 1 or 2
         while (looping)
         {
             //Prompts user to input of either logging in or registering a new account
             Console.WriteLine("Press the following Options\n[1] Login\n[2] Register");
             string entryInput = Console.ReadLine();
-
             //As long as the input is not null or empty and if it is either "1" or "2" otherwise prompt to input again
             if (!(string.IsNullOrEmpty(entryInput)) && (entryInput == "1" || entryInput == "2"))
             {
                 //Prompts user to register with a valid email and a secure password
-                //!Create a way to have it check with the Dictionary if the email exist already or not
                 if (Int32.Parse(entryInput) == 2)
                 {
-                    Console.WriteLine("Please input a valid email");
-                    string newuserName = Console.ReadLine();
-                    Console.WriteLine("Please input a secure password");
-                    string newpassword = Console.ReadLine();
+                    bool isregistered = Credentials.getRegistration();
 
-                    //Once entered it is entered into the database to be used as well as ending this while loop
-                    Credentials.Add(newuserName, newpassword);
-                    looping = false;
+                    if (isregistered)
+                    {
+                        Console.WriteLine("Registration was successful!\n" +
+                            "You will be prompt back to the previous menu.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Registration was unsuccessful you will be prompted to the previous menu");
+                    }
                 }
                 //Prompts the user to continue forward with the login
                 else
@@ -57,44 +60,22 @@ public class Program
                 Console.WriteLine("Please enter a valid input");
             }
         }
-        
         looping = true;
         Console.WriteLine("Welcome please enter your username and password");
-        
-        //This is the user login prompt to have user input their valid email and password
-        while (looping)
-        {
-            //Prompts user to input their username(email) and password associated with it
-            Console.Write("Username: ");
-            string userInput1 = Console.ReadLine();
-            Console.Write("Password: ");
-            string userInput2 = Console.ReadLine();
 
-            //Based on whether the input is valid will perform one of the if statements
-            //!Recreate the if statements in order to have it based off of the dictionary
-            //Nested if statement of the outside loop to check if the username exist
-
-            // if (Credentials[userInput1] == userInput2)
-            
-            if (Credentials.ContainsKey(userInput1) && Credentials[userInput1] == userInput2)
+            //This is the user login prompt to have user input their valid email and password
+            while (looping)
             {
-
-                Console.WriteLine("Credential Accepted!");
+                bool islogin = Credentials.getLogin();
+            if (islogin)
+            {
+                Console.WriteLine("Login attempt was Successful");
+                Console.WriteLine("GoodBye for now. I'm still building this app so hold your horses. But good job Pisith!");
                 looping = false;
-
-            }
-            else if ((!(Credentials.ContainsKey(userInput1)) && Credentials[userInput1] == userInput2) || ((Credentials.ContainsKey(userInput1)) && Credentials[userInput1] != userInput2))
-            {
-
-
-                Console.WriteLine("The username or password you entered was invalid please try again");
             }
             else
             {
-
-                Console.WriteLine("That username seems to not be registered. Would you like to register it?");
-                looping = false;
-
+                Console.WriteLine("Login was unsuccessful. You will be prompted to the previous menu");
             }
         }
     }
